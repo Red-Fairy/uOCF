fab490c637f53ce50598173508fd613ef753186a
Rundong Luo <redfairy@viscam1.stanford.edu>
2023-04-01 22:40:22 -0700

    update

--- scripts/validate_clevr_567.sh
+++ scripts/validate_clevr_567.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-DATAROOT=${1:-'/viscam/u/redfairy/datasets/clevr567/test'}
+DATAROOT=${1:-'/viscam/u/redfairy/datasets/clevr567/unseen'}
 PORT=${2:-12783}
-python test.py --dataroot $DATAROOT --n_scenes 500 --n_img_each_scene 4 \
+python test.py --dataroot $DATAROOT --n_scenes 100 --n_img_each_scene 4 \
     --checkpoints_dir 'checkpoints' --name 'clevr_567' --results_dir 'results' \
     --display_port $PORT --display_ncols 4 \
     --load_size 128 --input_size 64 --render_size 8 --frustum_size 128 \
     --n_samp 256 --z_dim 40 --num_slots 8 \
     --model 'uorf_eval_T' \
     --exp_id '0328-project-loadEncoder' --pos_emb \
-    --project --attn_iter 4 --testset_name regular_test \
+    --project --attn_iter 4 --testset_name unseen_test \
 # done
 echo "Done"
