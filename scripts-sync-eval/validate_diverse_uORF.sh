7f4f05e2319e6188a811b6fe9a6775ec7e8744ea
Rundong Luo <redfairy@viscam1.stanford.edu>
2023-04-14 19:46:59 -0700

    update script

--- scripts-sync-eval/validate_diverse_uORF.sh
+++ scripts-sync-eval/validate_diverse_uORF.sh
@@ -4,12 +4,10 @@ PORT=${2:-12783}
 python test.py --dataroot $DATAROOT --n_scenes 500 --n_img_each_scene 4 \
     --checkpoints_dir 'checkpoints' --name 'room_diverse' --results_dir 'results' \
     --display_port $PORT --display_ncols 4 \
-    --load_size 128 --render_size 8 --frustum_size 128 \
+    --load_size 128 --input_size 128 --render_size 8 --frustum_size 128 --bottom \
     --n_samp 256 --z_dim 64 --num_slots 5 \
-    --model 'uorf_eval_T' \
-    --sam_encoder --encoder_size 1024 \
-    --pos_emb --exp_id '0409-sam' \
-    --z_dim 64 \
-    --project --attn_iter 4 --testset_name trail_test \
+    --model 'uorf_eval' \
+    --pos_emb --exp_id '0410-uORF' \
+    --attn_iter 4 --testset_name regular_test \
 # done
 echo "Done"
