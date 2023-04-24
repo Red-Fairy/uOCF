10eeaaf742dc4249c8af1f15f1c72b8409c9047c
Rundong Luo <redfairy@viscam1.stanford.edu>
2023-04-24 06:15:26 -0700

    update script

--- scripts/eval_nvs_seg_chair.sh
+++ scripts/eval_nvs_seg_chair.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-DATAROOT=${1:-'/viscam/projects/uorf-extension/datasets/room_chair/test_ood_2.5'}
+DATAROOT=${1:-'/viscam/projects/uorf-extension/datasets/room_chair/test'}
 PORT=${2:-12783}
 PORT=8077
 python -m visdom.server -p $PORT &>/dev/null &
-python test.py --dataroot $DATAROOT --n_scenes 100 --n_img_each_scene 4 \
+python test.py --dataroot $DATAROOT --n_scenes 500 --n_img_each_scene 4 \
     --checkpoints_dir 'checkpoints' --name 'room_chair' --results_dir 'results' \
     --display_port $PORT --display_ncols 4 \
     --load_size 128 --input_size 64 --render_size 8 --frustum_size 128 \
     --n_samp 256 --z_dim 64 --num_slots 5 \
     --exp_id 'uORF-pretrained' \
-    --model 'uorf_eval'
-echo "Done"
+    --model 'uorf_eval' \
+    --testset_name 'test_regular' 
